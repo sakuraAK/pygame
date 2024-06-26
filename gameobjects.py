895fa0990b8f9f4eca4595fe0da9c045df6f1f27
Andrey <andrey.krutauz@gmail.com>
2024-06-25 20:39:18 -0400

    Breakout continued

--- gameobjects.py
+++ gameobjects.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import RED, SCREEN_WIDTH, SCREEN_HEIGHT, PAD_WIDTH
+from constants import RED, SCREEN_WIDTH, SCREEN_HEIGHT, PAD_WIDTH, BALL_WIDTH, BRICK_WIDTH, BRICK_HEIGHT, BRICK_HIT_CNT
 from math import sqrt, ceil
 
 class Pad:
@@ -23,4 +23,80 @@ class Pad:
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
-        # pygame.draw.rect(surface, RED, self.rect, 1)
+        pygame.draw.rect(surface, RED, self.rect, 1)
+
+
+class Ball:
+
+    def __init__(self, image, x, y):
+        self.image = image
+        self.rect = pygame.Rect(x, y, BALL_WIDTH, BALL_WIDTH)
+        self.dx = 2
+        self.dy = 2
+
+
+    def move(self, pad, bricks):
+
+    #  detect collision with the pad
+        if self.rect.colliderect(pad.rect):
+            self.dy = -self.dy
+
+
+
+    # detect go off the screen left
+
+        if self.rect.x < 0:
+            self.rect.x = 0
+            self.dx = -self.dx
+
+    # detect go off the screen right
+
+        if self.rect.x > SCREEN_WIDTH - BALL_WIDTH:
+            self.rect.x = SCREEN_WIDTH - BALL_WIDTH
+            self.dx = -self.dx
+
+
+        self.rect.y += self.dy
+        for brick in bricks:
+            if self.rect.colliderect(brick.rect):
+                if self.dy < 0:
+                    # hit brick from below
+                    self.rect.top = brick.rect.bottom
+                else:
+                    self.rect.bottom = brick.rect.top
+                self.dy = -self.dy
+                brick.update_hit()
+
+        self.rect.x += self.dx
+        for brick in bricks:
+            if self.rect.colliderect(brick.rect):
+                if self.dx > 0:
+                    #hit the brick from the left
+                    self.rect.right = brick.rect.left
+                else:
+                    self.rect.left = brick.rect.right
+                self.dx = -self.dx
+                brick.update_hit()
+
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+        pygame.draw.rect(surface, RED, self.rect, 1)
+
+
+class Brick(pygame.sprite.Sprite):
+    def __init__(self, images, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.hit_counter = 0
+        self.images = images
+        self.rect = pygame.Rect(x, y, BRICK_WIDTH, BRICK_HEIGHT)
+
+    def draw(self, surface):
+        surface.blit(self.images[self.hit_counter], self.rect)
+        pygame.draw.rect(surface, RED, self.rect, 1)
+
+    def update_hit(self):
+        if self.hit_counter < BRICK_HIT_CNT:
+            self.hit_counter += 1
+        else:
+            self.kill()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
-from constants import BG, SCREEN_WIDTH, SCREEN_HEIGHT, FPS, OBJ_SPEED
-from gameobjects import Pad
+from constants import BG, SCREEN_WIDTH, SCREEN_HEIGHT, FPS, OBJ_SPEED, BALL_SCALE, PANEL, WHITE, FONT_SIZE_MED, BLACK, \
+GAME_INFO_SECTION_HEIGHT, BRICK_WIDTH, BRICK_HEIGHT
+
+from gameobjects import Pad, Ball, Brick
 
 
 
@@ -10,9 +12,34 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Breakout")
 clock = pygame.time.Clock()
 
+level = 1
+score = 0
+lives = 3
+
+def scale_image(image, scale):
+    w = image.get_width()
+    h = image.get_height()
+    return pygame.transform.scale(image, (w * scale, h * scale))
+
+def draw_text(font, text, color, pos_x, pos_y, surface):
+    font_img = font.render(text, color, color)
+    surface.blit(font_img, pygame.Rect(pos_x, pos_y, 40, 40))
+
+
+def draw_game_info():
+    pygame.draw.rect(screen, PANEL, (0, 0, SCREEN_WIDTH, GAME_INFO_SECTION_HEIGHT))
+    pygame.draw.line(screen, WHITE, (0, GAME_INFO_SECTION_HEIGHT), (SCREEN_WIDTH, GAME_INFO_SECTION_HEIGHT))
+    draw_text(atari_font_medium, f"Level:{level}", BLACK, SCREEN_WIDTH // 2 - 100, 10, screen)
+    draw_text(atari_font_medium, f"Lives:{lives}", BLACK, 10, 10, screen)
+    draw_text(atari_font_medium, f"Score:{score}", BLACK, SCREEN_WIDTH - 200, 10, screen)
+
 # image processing
 pad_image = pygame.image.load("./assets/images/pad.png")
-
+ball_image = scale_image(pygame.image.load("./assets/images/ball.png"), BALL_SCALE)
+atari_font_medium = pygame.font.Font("assets/fonts/AtariClassic.ttf", FONT_SIZE_MED)
+brick_image_whole = scale_image(pygame.image.load("./assets/images/1_0.png"), 0.25)
+brick_image_cracked = scale_image(pygame.image.load("./assets/images/1_1.png"), 0.25)
+brick_images = [brick_image_whole, brick_image_cracked]
 
 run = True
 move_left = False
@@ -20,7 +47,15 @@ move_right = False
 move_up = False
 move_down = False
 
+# game objects
 pad = Pad(pad_image)
+ball = Ball(ball_image, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+bricks = pygame.sprite.Group()
+
+for i in range(SCREEN_WIDTH // BRICK_WIDTH):
+    brick = Brick(brick_images, 0 + i * BRICK_WIDTH, GAME_INFO_SECTION_HEIGHT + 5)
+    bricks.add(brick)
+
 
 
 
@@ -28,8 +63,10 @@ while run:
     clock.tick(FPS)
     screen.fill(BG)
 
-#     game logic
 
+
+
+#     game logic
     # calculate movement
     dx = 0
     if move_left:
@@ -38,9 +75,17 @@ while run:
         dx = OBJ_SPEED
 
     pad.move(dx)
+    ball.move(pad, bricks)
+
+    for brick in bricks:
+        brick.draw(screen)
+
+    # bricks.draw(screen)
 
     pad.draw(screen)
+    ball.draw(screen)
 
+    draw_game_info()
     # event handling
 
     for e in pygame.event.get():
